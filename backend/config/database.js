6fbc187c795a62f1f38ef88eefc62a862d7b2fd8
umair <umair391220@gmail.com>
2025-10-20 12:54:53 +0530

    corrects env naming

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,9 +6,9 @@ dotenv.config();
 const sequelize = new Sequelize({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'carpooling_app',
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME || 'carpool',
+  username: process.env.DB_USER || 'umair',
+  password: process.env.DB_PASSWORD || 'Umair-12',
   dialect: 'postgres',
   logging: process.env.NODE_ENV === 'development' ? console.log : false,
   pool: {
