ab51fbca4d032a98cbe46c2d67f83a64657ad280
umair <umair391220@gmail.com>
2025-10-20 16:16:47 +0530

    removes redundant dotenv config injection

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,6 @@
 import { Sequelize } from 'sequelize'
 import dotenv from 'dotenv';
 
-dotenv.config();
-
 const sequelize = new Sequelize({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 5432,
