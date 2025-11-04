ed801a3c0763ca07516361dff62e4a36fe474d95
umair <umair391220@gmail.com>
2025-11-05 00:50:51 +0530

    adds my_booking drawer

--- frontend/CarPoolApp/app/(tabs)/(drawer)/_layout.tsx
+++ frontend/CarPoolApp/app/(tabs)/(drawer)/_layout.tsx
@@ -1,17 +1,16 @@
 import { Stack } from 'expo-router';
 import { Drawer } from 'expo-router/drawer';
-// import { GestureHandlerRootView } from 'react-native-gesture-handler';
-// const Drawer = createDrawerNavigator(); #manually create drawer using react native - hard pass
 
 export default function RootLayout() {
   return (
     <Drawer>
       <Drawer.Screen name="home_screen" options={{ title: 'Home' }} />
-      <Drawer.Screen name="new_ride" options={{ title: 'Start a New Ride' }} />
-      <Drawer.Screen name="existing_rides" options={{ title: 'Join a Ride' }} />
+      <Drawer.Screen name="existing_rides" options={{ title: 'Available Rides' }} />
+      <Drawer.Screen name="my_bookings" options={{ title: 'My Bookings' }} />
+      <Drawer.Screen name="new_ride" options={{ title: 'Create New Ride' }} />
       <Drawer.Screen name="about" options={{ title: 'About' }} />
       <Drawer.Screen name="user_profile" options={{ title: 'User Profile' }}/>
-      <Drawer.Screen name = "payments" options={{ title: 'Payment History' }}/>
+      <Drawer.Screen name="payments" options={{ title: 'Payment History' }}/>
     </Drawer>
   );
 }
